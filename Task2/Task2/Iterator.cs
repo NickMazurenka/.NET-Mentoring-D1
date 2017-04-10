7a8c7387988eca0a79bc15ea73004146f2094696
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2017-04-10 13:36:53 +0300

    Add Task2 base code

---
+++ Task2/Task2/Iterator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Task2
+{
+	public class Iterator<T> : IEnumerable
+	{
+		private readonly List<T> _items = new List<T>();
+
+		public void Add(T item)
+		{
+			_items.Add(item);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			for (int i = 0; i < _items.Count; i++)
+			{
+				yield return _items[i];
+			}
+		}
+	}
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task2
+{
+	class Program
+	{
+		static void Main()
+		{
+			var test = new Iterator<string>();
+			test.Add("test1");
+			test.Add("test2");
+
+			foreach (var item in test)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
