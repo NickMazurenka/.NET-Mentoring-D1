046de39fa23fe01042e2cdc6f08805df29a1b3b7
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2017-04-17 13:43:10 +0300

    Add Task3

---
+++ Task3/Task3/City.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Task3
+{
+	enum City
+	{
+		[Description("The one with Big Ben tower")]
+		London,
+		[Description("The one with big park")]
+		NewYork,
+		[Description("The one with National Library")]
+		Minsk
+	}
+}
---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Task3
+{
+	class Program
+	{
+		private static string GetEnumDescription(Enum value)
+		{
+			var fi = value.GetType().GetField(value.ToString());
+			var attributes = (DescriptionAttribute[]) fi.GetCustomAttributes(
+				typeof(DescriptionAttribute),
+				false);
+			return attributes.Length > 0 ? attributes[0].Description : value.ToString();
+		}
+
+		static void Main()
+		{
+			var t = City.Minsk;
+			var d = GetEnumDescription(t);
+			Console.WriteLine("Enum value: " + t);
+			Console.WriteLine("Enum description: " + d);
+
+		}
+	}
+}
