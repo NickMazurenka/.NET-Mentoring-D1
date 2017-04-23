0ab06ab4bd97d71e225d88903a8d287bedce6e50
SexPixels <nik.mazurenko@gmail.com>
2017-04-23 23:26:18 +0300

    Add base code for Task4

---
+++ Task4/StringToIntegerConverter/StringToIntConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StringToIntegerConverter
+{
+    public class StringToIntConverter
+    {
+        public static int Convert(string s)
+        {
+            // Check input data
+            if (s == null)
+                throw new ArgumentNullException($"Conevrted string {nameof(s)} cannot be null");
+
+            var result = 0;
+            var index = 0;
+            var positive = true;
+            if (s[0] == '-')
+            {
+                positive = false;
+                index++;
+            }
+            else if (s[0] == '+')
+                index++;
+
+            for (; index < s.Length; index++)
+            {
+                if (s[index] > '9' || s[index] < '0')
+                    throw new FormatException($"Invalid character at position {index}");
+                try
+                {
+                    result = result * 10 + (s[index] - '0');
+                }
+                catch (OverflowException e)
+                {
+                    throw new OverflowException($"Overflow when ", e);
+                }
+            }
+
+            return result * (positive ? 1 : -1);
+        }
+    }
+}
---
+++ Task4/StringToIntegerConverterTests/StringToIntConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringToIntegerConverter;
+
+namespace StringToIntegerConverterTests
+{
+    [TestClass]
+    public class StringToIntConverterTests
+    {
+        [TestMethod]
+        public void Convert_FiveDigitInt_Converted()
+        {
+            // Arrange
+            var s = "12345";
+
+            // Act
+            var r = StringToIntConverter.Convert(s);
+
+            // Assert
+            Assert.AreEqual(r, int.Parse(s));
+        }
+    }
+}
