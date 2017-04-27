7f581cb6fbbde7942e835e50b2553bc911d6c59e
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2017-04-27 18:20:31 +0300

    Add Task5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task5
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var type = typeof(Person);
+            var listType = typeof(List<>).MakeGenericType(type);
+            var list = (IList)Activator.CreateInstance(listType);
+
+            for (int i = 0; i < 5; i++)
+                list.Add(new Person
+                {
+                    Name = "Kim",
+                    Surname = "Kuan_" + i
+                });
+
+            foreach (var person in list)
+            {
+                var p = person as Person;
+                Console.WriteLine($"{p.Name} {p.Surname}");
+            }
+        }
+    }
+}
