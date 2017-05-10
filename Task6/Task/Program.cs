086e01ceeefe3276c2807e110831bab4089d6247
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2017-05-10 18:42:12 +0300

    Add template for Task6

---
+++ Task6/Task/Program.cs
@@ -0,0 +1,35 @@
+﻿// Copyright © Microsoft Corporation.  All Rights Reserved.
+// This code released under the terms of the 
+// Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.)
+//
+//Copyright (C) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+// See the ReadMe.html for additional information
+namespace Task
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var harnesses = new List<SampleHarness>();
+			
+			harnesses.Add(new LinqSamples());
+            harnesses.Add(new TaskSamples());
+						
+			Application.EnableVisualStyles();
+				
+			using (SampleForm form = new SampleForm("HomeWork", harnesses))
+			{
+				form.ShowDialog();
+			}
+		}
+	}
+}
---
+++ Task6/Task/TaskSamples.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Task.Data;
+
+namespace Task
+{
+    [Title("LINQ Module tasks")]
+    [Prefix("Linq")]
+    class TaskSamples : SampleHarness
+    {
+        private readonly DataSource _dataSource = new DataSource();
+
+        [Category("Hometask")]
+        [Title("Task 1")]
+        [Description("Выдайте список всех клиентов, чей суммарный оборот (сумма всех заказов) превосходит некоторую величину X")]
+        public void Linq1()
+        {
+            var x = 20000m;
+
+            var clients = _dataSource.Customers.Where(c => c.Orders.Sum(o => o.Total) > x);
+
+            foreach (var client in clients)
+                Console.WriteLine($@"ID {client.CustomerID} SUM {client.Orders.Sum(o => o.Total)}");
+        }
+    }
+}
