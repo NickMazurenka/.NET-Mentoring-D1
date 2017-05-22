d728e6df496dc53997c26f434fb3be8b27a82b90
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2017-05-22 20:17:52 +0300

    Add Dapper Examples

---
+++ Task7/Northwind.DAL/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperExample
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
+}
---
+++ Task7/Northwind.DAL/ExpensiveProduct.cs
@@ -0,0 +1,8 @@
+﻿namespace DapperExample
+{
+    public class ExpensiveProduct
+    {
+        public string TenMostExpensiveProducts { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
