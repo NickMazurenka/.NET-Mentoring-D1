413a882f4490c70d567046f72cf402c20ea5e7e9
Mikalai Mazurenka <Mikalai_Mazurenka@epam.com>
2017-06-26 17:38:14 +0300

    Add serializarion task

---
+++ T10-Serialization/T10-Serialization/Catalog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace T10_Serialization
+{
+    [XmlType("catalog", Namespace = "http://library.by/catalog")]
+    [XmlRoot(Namespace = "http://library.by/catalog", IsNullable = false)]
+    public class Catalog
+    {
+        [XmlElement("book")]
+        public CatalogBook[] Book { get; set; }
+
+        [XmlAttribute(DataType = "date")]
+        public DateTime Date { get; set; }
+    }
+
+    [XmlType("book", Namespace = "http://library.by/catalog")]
+    public class CatalogBook
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlElement("isbn")]
+        public string Isbn { get; set; }
+
+        [XmlElement("author")]
+        public string Author { get; set; }
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        [XmlElement("genre")]
+        public string Genre { get; set; }
+
+        [XmlElement("publisher")]
+        public string Publisher { get; set; }
+
+        [XmlElement("publish_date", DataType = "date")]
+        public DateTime PublishDate { get; set; }
+
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        [XmlElement("registration_date", DataType = "date")]
+        public DateTime RegistrationDate { get; set; }
+    }
+}
---
+++ T10-Serialization/T10-Serialization/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace T10_Serialization
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var serializer = new XmlSerializer(typeof(Catalog));
+            Catalog catalog;
+
+            // Deserialize
+            using (var reader = new StreamReader("books.xml"))
+                catalog = (Catalog) serializer.Deserialize(reader);
+
+            // Serialize
+            using (var stringWriter = new StreamWriter("test.xml"))
+                using (var writer = XmlWriter.Create(stringWriter))
+                    serializer.Serialize(writer, catalog);
+        }
+    }
+}
